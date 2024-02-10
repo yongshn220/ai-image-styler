55fa27b5738a30464bca42881a55ec161a21652b
Yongjung Shin <yongshn220@gmail.com>
2024-02-10 18:38:26 -0500

    minor change

--- components/footer/how-to-use.tsx
+++ components/footer/how-to-use.tsx
@@ -10,7 +10,7 @@ export function HowToUsePage() {
             <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">How to Use</h1>
             <p
               className="text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              Using Keywarts.com is straightforward and designed to make your creative process as seamless as possible. Here's how you can start transforming your art with AI:
+              Using Keywarts.com is straightforward and designed to make your creative process as seamless as possible. Here is how you can start transforming your art with AI:
             </p>
           </div>
         </div>
@@ -29,7 +29,7 @@ export function HowToUsePage() {
               <h2 className="text-2xl font-bold">2. Copy and Use Prompts</h2>
               <p className="text-gray-500 dark:text-gray-400">
                 Once you find an image or keyword that resonates with your vision, simply copy the prompt. These
-                prompts are tailored to be used directly with Midjourney's AI, ensuring compatibility and ease of use.
+                prompts are tailored to be used directly with Midjourney AI, ensuring compatibility and ease of use.
               </p>
             </div>
             <div className="grid gap-2">
@@ -43,7 +43,7 @@ export function HowToUsePage() {
             <div className="grid gap-2">
               <h2 className="text-2xl font-bold">4. Search on Midjourney</h2>
               <p className="text-gray-500 dark:text-gray-400">
-                If you prefer, you can also use the keywords provided on our site to search directly on Midjourney's platform. This allows for a more flexible approach to finding the perfect inspiration for your projects.
+                If you prefer, you can also use the keywords provided on our site to search directly on Midjourney platform. This allows for a more flexible approach to finding the perfect inspiration for your projects.
               </p>
             </div>
           </div>
