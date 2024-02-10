1580d3fabee9ef279a25832f97e612e3e0ae160c
Yongjung Shin <yongshn220@gmail.com>
2024-02-10 00:23:02 -0500

    Add | vercel analytics

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -15,21 +15,6 @@ export default function Nav() {
         <span className="text-xl">Keywarts</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
-        {/*<Link className="text-sm font-medium hover:underline underline-offset-4" href="#">*/}
-        {/*  Home*/}
-        {/*</Link>*/}
-        {/*<Link className="text-sm font-medium hover:underline underline-offset-4" href="#">*/}
-        {/*  Destinations*/}
-        {/*</Link>*/}
-        {/*<Link className="text-sm font-medium hover:underline underline-offset-4" href="#">*/}
-        {/*  Tours*/}
-        {/*</Link>*/}
-        {/*<Link className="text-sm font-medium hover:underline underline-offset-4" href="#">*/}
-        {/*  About*/}
-        {/*</Link>*/}
-        {/*<Link className="text-sm font-medium hover:underline underline-offset-4" href="#">*/}
-        {/*  Contact*/}
-        {/*</Link>*/}
       </nav>
     </header>
   )
